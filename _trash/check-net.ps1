5a4873b165a01eb663f60bc1771fd69acbdb5522
Yuchen Zhang <1976693250@qq.com>
2026-08-06 01:43:43 +0800

    cleanup: remove ignored/temp dirs and tracked artifacts
    - Delete _trash/ and tracked contents
    - Delete grasping/logs/ tracked artifacts
    - Delete yolo-pipeline dataset README/label txts and generated .pt models
    - Remove tracked __pycache__ files in robot-sdk/
    - Remove tracked .DS_Store files
    - Move _audit-cache/ and papers/ to local backup outside repo

--- _trash/check-net.ps1
+++
@@ -1,2 +0,0 @@
-Get-NetAdapter -Physical | Where-Object { $_.Status -eq 'Up' } | Select-Object Name,InterfaceDescription,LinkSpeed
-Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -notlike '*Loopback*' -and $_.InterfaceAlias -notlike '*vEthernet*' } | Select-Object InterfaceAlias,IPAddress,PrefixLength
